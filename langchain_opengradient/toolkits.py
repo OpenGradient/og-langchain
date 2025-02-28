ddde1fc2abf264704340314671c6b82eee9a9b87
kylexqian <kylexqian@gmail.com>
2025-02-28 04:31:08 -0500

    Add skeleton for Opengradient toolkit

--- langchain_opengradient/toolkits.py
+++ langchain_opengradient/toolkits.py
@@ -1,9 +1,12 @@
 """OpenGradient toolkits."""
 
-from typing import List
+from typing import List, Callable, Type, Optional
+from pydantic import BaseModel
+import os
 
 from langchain_core.tools import BaseTool, BaseToolkit
-
+from opengradient.alphasense import create_run_model_tool, create_read_workflow_tool, ToolType
+import opengradient as og
 
 class OpenGradientToolkit(BaseToolkit):
     # TODO: Replace all TODOs in docstring. See example docstring:
@@ -67,6 +70,75 @@ class OpenGradientToolkit(BaseToolkit):
 
     """  # noqa: E501
 
+    def __init__(self):
+        # Initialize OpenGradient client
+        private_key = private_key or os.getenv("OPENGRADIENT_PRIVATE_KEY")
+        if not private_key:
+            raise ValueError("OPENGRADIENT_PRIVATE_KEY environment variable is not set")
+
+        og.init(private_key=private_key, email=None, password=None)
+
+        # List of tool (Expected to be of type BaseTool)
+        self.tools = []
+
     # TODO: This method must be implemented to list tools.
     def get_tools(self) -> List[BaseTool]:
         raise NotImplementedError()
+    
+    def add_tool(self, tool: BaseTool) -> None:
+        """Add tool to the list of tools for the OpenGradient Agentkit."""
+        # Maybe add a check here
+        self.tools.append(tool)
+
+    def create_run_model_tool(self,
+                              model_cid: str,
+                              tool_name: str,
+                              input_getter: Callable,
+                              output_formatter: Callable[..., str] = lambda x: x,
+                              input_schema: Optional[Type[BaseModel]] = None,
+                              tool_description: str = "Executes the given ML model",
+                              inference_mode: og.InferenceMode = og.InferenceMode.VANILLA) -> BaseTool:
+        """
+        Wrapper for create_run_model_tool from OpenGradient AlphaSense library.
+        
+        This function creates a langchain compatible tool to run inferences on the OpenGradient network. 
+
+        Example usage:
+            from og_langchain.toolkits import OpenGradientToolkit
+
+
+        """
+        tool = create_run_model_tool(tool_type=ToolType.LANGCHAIN, 
+                                     model_cid=model_cid, 
+                                     tool_name=tool_name, 
+                                     input_getter=input_getter, 
+                                     output_formatter=output_formatter, 
+                                     input_schema=input_schema,
+                                     tool_description=tool_description,
+                                     inference_mode=inference_mode)
+        
+        return tool
+
+    def create_read_workflow_tool(self,
+                                  workflow_contract_address: str,
+                                  tool_name: str,
+                                  tool_description: str,
+                                  output_formatter: Callable[..., str] = lambda x: x) -> BaseTool:
+        """
+        Wrapper for create_read_workflow_tool from OpenGradient AlphaSense library.
+        
+        This function creates a langchain compatible tool to read workflows on the OpenGradient network. 
+
+        Example usage:
+            from og_langchain.toolkits import OpenGradientToolkit
+            
+            toolkit = OpenGradientToolkit()
+            toolkit.create_read_workflow_tool()
+        """
+        tool = create_read_workflow_tool(tool_type=ToolType.LANGCHAIN,
+                                         workflow_contract_address=workflow_contract_address,
+                                         tool_name=tool_name,
+                                         tool_description=tool_description,
+                                         output_formatter=output_formatter)
+        
+        return tool
